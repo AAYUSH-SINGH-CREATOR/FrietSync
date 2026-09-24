6befc9567deabb314aaa1cafab984009cf33673d
aviralumrao <aviral0419@gmail.com>
2026-09-25 01:49:46 +0530

    Name update

--- backend/src/main/java/com/frietsync/backend/BackendApplication.java
+++ backend/src/main/java/com/frietsync/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.syncvarq.backend;
+package com.frietsync.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/frietsync/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/frietsync/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.syncvarq.backend;
+package com.frietsync.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
